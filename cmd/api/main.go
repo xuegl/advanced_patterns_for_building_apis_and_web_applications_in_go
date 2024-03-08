87b65081bd09c99478e472f30b9faba8ae6b7919
xuegang.leng <xuegang.leng@qq.com>
2024-03-08 23:03:54 +0800

    init repo

---
+++ cmd/api/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", app.config.port),
+		Handler:      app.routes(),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  time.Second * 10,
+		WriteTimeout: time.Second * 30,
+	}
+
+	logger.Printf("starting %s server on %s", app.config.env, srv.Addr)
+	err := srv.ListenAndServe()
+	logger.Fatal(err)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module xuegl/advanced_patterns_for_building_apis_and_web_applications_in_go
+
+go 1.22.1
+
+require github.com/julienschmidt/httprouter v1.3.0
